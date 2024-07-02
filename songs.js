a6262439e99cf3f9ec1a2ddba747a9ffb9a2cb58
Tanpreet <150704059+Tanpreet07@users.noreply.github.com>
2024-07-02 11:45:45 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,221 @@
+exports.songs=[
+    {
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+        "title": "Karan Aujla",
+        "description": "karan Aujla New Album",
+        "audio": [
+            {
+                "titlename":"100 Million",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/100Million.mp3"
+            },
+            {
+                "titlename":"On top",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/ontop.mp3"
+            },
+            {
+                "titlename":"Dont Worry",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/DontWorry.mp3"
+            },
+            {
+                "titlename":"Fallin Apart",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/FallinApart.mp3"
+            },
+            {
+                "titlename":"Let Em Play",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/Let-Em-Play.mp3"
+            },
+            {
+                "titlename":"No Need",
+                "singar":"Karan Aujla",
+                "songimg":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZkbkmP-h98wDqQpFKeoTp-aQEfoVyhcNRg&s",
+                "songurl":"./audio/Karan-aujla/No-Need.mp3"
+            }
+        ]
+    },
+    {
+        "img": "https://assets.vogue.in/photos/650834d7d70eb7f1f666fd29/2:3/w_2560%2Cc_limit/Snapinsta.app_343111618_237478218937905_980114793430031196_n_1080.jpg",
+        "title": "Diljit Dosanjh",
+        "description": "Daljit Dosanjh new album",
+        "audio": [
+            {
+                "titlename":"G.O.T.A",
+                "singar":"Diljit Dosanjh",
+                "songimg":"https://assets.vogue.in/photos/650834d7d70eb7f1f666fd29/2:3/w_2560%2Cc_limit/Snapinsta.app_343111618_237478218937905_980114793430031196_n_1080.jpg",
+                "songurl":"./audio/Diljitdosanjh/GOTA.mp3"
+            },
+            {
+                "titlename":"Born To Shine",
+                "singar":"Diljit Dosanjh",
+                "songimg":"https://assets.vogue.in/photos/650834d7d70eb7f1f666fd29/2:3/w_2560%2Cc_limit/Snapinsta.app_343111618_237478218937905_980114793430031196_n_1080.jpg",
+                "songurl":"./audio/Diljitdosanjh/Born-To-Shine.mp3"
+            },
+            {
+                "titlename":"Case",
+                "singar":"Diljit Dosanjh",
+                "songimg":"https://assets.vogue.in/photos/650834d7d70eb7f1f666fd29/2:3/w_2560%2Cc_limit/Snapinsta.app_343111618_237478218937905_980114793430031196_n_1080.jpg",
+                "songurl":"./audio/Diljitdosanjh/Case.mp3"
+            },
+            {
+                "titlename":"Clash",
+                "singar":"Diljit Dosanjh",
+                "songimg":"https://assets.vogue.in/photos/650834d7d70eb7f1f666fd29/2:3/w_2560%2Cc_limit/Snapinsta.app_343111618_237478218937905_980114793430031196_n_1080.jpg",
+                "songurl":"./audio/Diljitdosanjh/Clash.mp3"
+            }
+        ]
+    },
+    {
+        "img": "https://wallpapers.com/images/featured/arijit-singh-q307hnimzo1z26ct.jpg",
+        "title": "Arijit Singh",
+        "description": "Arijit Singh new album",
+        "audio": [
+            {
+                "titlename":"Heeriye Heeriye Aa",
+                "singar":"Arijit Singh",
+                "songimg":"https://wallpapers.com/images/featured/arijit-singh-q307hnimzo1z26ct.jpg",
+                "songurl":"./audio/Arijit singh/Heeriye-Heeriye-Aa.mp3"
+            },
+            {
+                "titlename":"Kesariya Remix",
+                "singar":"Arijit Singh",
+                "songimg":"https://wallpapers.com/images/featured/arijit-singh-q307hnimzo1z26ct.jpg",
+                "songurl":"./audio/Arijit singh/Kesariya-Remix.mp3"
+            },
+            {
+                "titlename":"Khamoshiyan",
+                "singar":"Arijit Singh",
+                "songimg":"https://wallpapers.com/images/featured/arijit-singh-q307hnimzo1z26ct.jpg",
+                "songurl":"./audio/Arijit singh/Khamoshiyan.mp3"
+            },
+            {
+                "titlename":"O Sajni Re",
+                "singar":"Arijit Singh",
+                "songimg":"https://wallpapers.com/images/featured/arijit-singh-q307hnimzo1z26ct.jpg",
+                "songurl":"./audio/Arijit singh/O-Sajni-Re.mp3"
+            }            
+        ]
+    },
+    {
+        "img": "./images/PartyHits.jpg",
+        "title": "Party Hits",
+        "description": "Best party Hits",
+        "audio": [
+            {
+                "titlename":"Party All Night",
+                "singar":"Yo Yo Honey Singh",
+                "songimg":"./images/PartyHits.jpg",
+                "songurl":"./audio/Partysongs/Party-All-Night.mp3"
+            },
+            {
+                "titlename":"Sunny Sunny",
+                "singar":"Yo Yo Honey Singh",
+                "songimg":"./images/PartyHits.jpg",
+                "songurl":"./audio/Partysongs/Sunny-sunny.mp3"
+            }          
+        ]
+    },
+    {
+        "img": "https://i1.sndcdn.com/avatars-GBhEb4pyHIRKyPfO-9aDwAw-t240x240.jpg",
+        "title": "Shubh",
+        "description": "Shubh New Album",
+        "audio": [
+            {
+                "titlename":"One Love",
+                "singar":"Shubh",
+                "songimg":"https://i1.sndcdn.com/avatars-GBhEb4pyHIRKyPfO-9aDwAw-t240x240.jpg",
+                "songurl":"./audio/Shubh/One-Love.mp3"
+            },
+            {
+                "titlename":"Still Rollin",
+                "singar":"Shubh",
+                "songimg":"https://i1.sndcdn.com/avatars-GBhEb4pyHIRKyPfO-9aDwAw-t240x240.jpg",
+                "songurl":"./audio/Shubh/Still-Rollin.mp3"
+            }
+        ]
+    },
+    {
+        "img": "./images/Divine.png",
+        "title": "Divine",
+        "description": "Divine New Album",
+        "audio": [
+            {
+                "titlename":"Baazigar",
+                "singar":"Divine",
+                "songimg":"./images/Divine.png",
+                "songurl":"./audio/Divine/Baazigar.mp3"
+            },                  
+            {
+                "titlename":"Blessings",
+                "singar":"Divine",
+                "songimg":"./images/Divine.png",
+                "songurl":"./audio/Divine/Blessings.mp3"
+            },                  
+            {
+                "titlename":"Satya",
+                "singar":"Divine",
+                "songimg":"./images/Divine.png",
+                "songurl":"./audio/Divine/Satya.mp3"
+            }               
+        ]
+    },
+    {
+        "img": "./images/Punjabi-sad-songs.png",
+        "title": "Punjabi Sad Hits",
+        "description": "Best Punjabi Sad Hits",
+        "audio": [
+            {
+                "titlename":"Phulkari",
+                "singar":"Ranjit Bawa",
+                "songimg":"./images/Punjabi-sad-songs.png",
+                "songurl":"./audio/Punjabisadsongs/Phulkari.mp3"
+            }               
+        ]
+    },
+    {
+        "img": "./images/Sidhu-Moosewala.png",
+        "title": "Sidhu Moosewala",
+        "description": "Sidhu Moosewala New Album",
+        "audio": [
+            {
+                "titlename":"295",
+                "singar":"Sidhu Moosewala",
+                "songimg":"./images/Sidhu-Moosewala.png",
+                "songurl":"./audio/Sidhu-moosewala/295.mp3"
+            },
+            {
+                "titlename":"Game",
+                "singar":"Sidhu Moosewala",
+                "songimg":"./images/Sidhu-Moosewala.png",
+                "songurl":"./audio/Sidhu-moosewala/Game.mp3"
+            }
+        ] 
+    },
+    {
+        "img": "./images/SadHits.png",
+        "title": "Hindi Sad Hits",
+        "description": "Top Hindi Sad Hits",
+        "audio": [
+            {
+                "titlename":"Agar Tum Saath Ho",
+                "singar":"Alka Yagnik-Arijit Singh",
+                "songimg":"./images/SadHits.png",
+                "songurl":"./audio/Hindisadsongs/Agar-Tum-Saath-Ho.mp3"
+            },          
+            {
+                "titlename":"Tu Koi Aur Hai",
+                "singar":"A-R-Rahman",
+                "songimg":"./images/SadHits.png",
+                "songurl":"./audio/Hindisadsongs/Tu-Koi-Aur-Hai.mp3"
+            }       
+        ]
+    }   
+]
